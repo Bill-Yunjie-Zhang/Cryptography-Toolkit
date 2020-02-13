102e5a3440f0c61ecf23867d2488259c4ca52922
Bill <207559339@qq.com>
2020-02-13 19:03:50 +0800

    encrypt text

--- encryptionLib/caesar.js
+++ encryptionLib/caesar.js
@@ -1,5 +1,3 @@
-const readyText = require("./components/readyText")
-const readyString = require("./components/readyString")
 const readyKey = require("./components/readyKey")
 const stats = require("./components/stats")
 const standardDeviation = require("./components/standardDeviation")
@@ -189,7 +187,20 @@ const forceCrack = (txt) => {
     return arr1.join("\n")
 }
 
+const justText = (txt) => {
+    let txtArr = []
+    let al = "abcdefghijklmnopqrstuvwxyz"
+    let alArray = al.split("")
+    txt.replace(/ /g, "").toLowerCase().split("").forEach(e => {
+        if (alArray.find(ele => ele === e)){
+            txtArr.push(e)
+        }
+    });
+    return txtArr.join("")
+}
+
 const encryptionLib = {
+    justText: justText,
     caesarEncryption: caesarEncryption,
     caesarDecryption: caesarDecryption,
     caesarEncryptionWithKey: caesarEncryptionWithKey,
@@ -203,6 +214,8 @@ const encryptionLib = {
 
 module.exports = encryptionLib
 /////////////////////////////////////////////////////////////////// tests ///////////////////////////////////////////////////////////////////
+
+let text = "In my younger and more vulnerable years my father gaveme some advice that I’ve been turning over in my mindever since.‘Whenever you feel like criticizing any one,’ he told me,‘just remember that all the people in this world haven’t hadthe advantages that you’ve had.’He didn’t say any more but we’ve always been unusuallycommunicative in a reserved way, and I understood that hemeant a great deal more than that. In consequence I’m inclined to reserve all judgments, a habit that has opened upmany curious natures to me and also made me the victimof not a few veteran bores. The abnormal mind is quick todetect and attach itself to this quality when it appears in anormal person, and so it came about that in college I wasunjustly accused of being a politician, because I was privyto the secret griefs of wild, unknown men. Most of the confidences were unsought—frequently I have feigned sleep,preoccupation, or a hostile levity when I realized by someunmistakable sign that an intimate revelation was quivering on the horizon—for the intimate revelations of youngmen or at least the terms in which they express them areusually plagiaristic and marred by obvious suppressions.Reserving judgments is a matter of infinite hope."
 
 /** test for caesarEncryption & caesarDecryption function */
 // let step = 100
@@ -237,3 +250,7 @@ module.exports = encryptionLib
 /** test for cracking by force */
 // let cipher = "Hel"
 // console.log(forceCrack(cipher))
+
+/** test for encrypting just text */
+// console.log(caesarEncryption(justText(text), 10))
+// console.log(caesarDecryption(caesarEncryption(justText(text), 10), 10))
